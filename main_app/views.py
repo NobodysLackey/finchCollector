b9eede75845b0b5341bccdcd3e69cef60fa978b6
Michael Lackey <michael.g.lackey@gmail.com>
2020-03-16 16:09:51 -0500

    create Super User

--- main_app/views.py
+++ main_app/views.py
@@ -1,24 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-class Finch:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, species, description, age):
-    self.name = name
-    self.species = species
-    self.description = description
-    self.age = age
-
-finches = [
-  Finch('Tweety', 'Saffron Finch', 'Golden yellow', 3),
-  Finch('Barry', 'Strawberry Finch', 'Deep red and maroon with white spots', 0),
-  Finch('Rainbow', 'Gouldian Finch', 'Bright red, cyan, violet, green, and gold', 4)
-]
+from .models import Finch
 
 def home(request):
   return HttpResponse('<h1>Hello Finch</h1>')
 
 def about(request):
-    return render(request, 'about.html')
+  return render(request, 'about.html')
 
 def finches_index(request):
-    return render(request, 'finches/index.html', {'finches': finches})
+  finches = Finch.objects.all()
+  return render(request, 'finches/index.html', {'finches': finches})
