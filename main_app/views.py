1a96006b290063dd843173bb111584918b945b1c
Michael Lackey <michael.g.lackey@gmail.com>
2020-03-17 12:23:32 -0500

    add full CRUD functionality

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
+
+class FinchCreate(CreateView):
+  model = Finch
+  fields = '__all__'
+  success_url = '/finches/'
+
+class FinchUpdate(UpdateView):
+  model = Finch
+  # Let's disallow the renaming of a cat by excluding the name field!
+  fields = ['species', 'description', 'age']
+
+class FinchDelete(DeleteView):
+  model = Finch
+  success_url = '/cats/'
 
 def home(request):
   return render(request, 'home.html')
